ad56742803c026c52a16c47bfe7e0c8342a34169
Pozitiwchik <noxe2019@mail.ru>
2023-06-18 23:58:00 +0300

    created 2 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthorizationComponent} from "./pages/authorization/authorization.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'authorization',
+    loadChildren: () =>
+      import('./pages/authorization/authorization.module').then(
+        (m) => m.AuthorizationModule
+      )
+  },
+  {
+    path: 'registration',
+    loadChildren: () =>
+      import('./pages/registration/registration.module').then(
+        (m) => m.RegistrationModule
+      )
+  },
+  {
+    path: '**',
+    redirectTo: '/authorization',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
